63a8026368a2936b97df45387c43c4dcf7af71ba
Ravi Kiran <bychancebychoice@gmail.com>
2023-05-12 04:31:49 +0530

    feat: new commit. this is third commit. fixes #66

--- src/cardholder/cardholder.service.ts
+++ src/cardholder/cardholder.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CardHolder } from './entities/cardholder.entity';
 import * as soap from 'soap';
 import { CardHolderRepository } from './cardholder.repository';
+
+const traceId = () => `RI007${new Date().getTime()}`;
+const wsdlURL =
+  'https://sandbox.flexpay.co.za/fnds-webservices/v2.0/FlexpayFicaStandardService?wsdl';
+const soapifiedRequest = (req: any) => {
+  const creds = {
+    traceId: traceId(),
+    clientUsername: 'RETAIL_INSIGHTS_TEST',
+    clientPassword: 'R3t@!1T3sTi8',
+    cardId: '3525223',
+    customerId: '2985'
+  };
+  const reqData = { ...req };
+  delete reqData.email;
+  return { arg0: { ...creds, ...reqData } };
+};
 
 @Injectable()
 export class CardHolderService {
@@ -12,29 +28,30 @@ export class CardHolderService {
     private readonly cardHolderRepository: CardHolderRepository
   ) {}
 
-  async create(cardHolderData: Partial<CardHolder>): Promise<CardHolder> {
-    const soapClient = await soap.createClientAsync(
-      'https://your-soap-api-endpoint?wsdl'
-    );
+  async create(cardHolderData: any) {
+    const client = await soap.createClientAsync(wsdlURL);
 
-    // Prepare the input parameters for the SOAP API call
-    const soapInput = {
-      // Fill in the input parameters based on your SOAP API requirements
+    const soapifiedReq = soapifiedRequest(cardHolderData);
+
+    const res = await client.createCardholderAsync(soapifiedReq);
+    console.log('response');
+
+    console.log(res);
+
+    const cardHolder: Partial<CardHolder> = {
+      firstName: cardHolderData.cardholder.firstName,
+      lastName: cardHolderData.cardholder.surname,
+      phoneNumber: cardHolderData.cardholder.phone,
+      passportId: cardHolderData.cardholder.primaryIdentity.number,
+      additionalData: cardHolderData,
+      dob: cardHolderData.cardholder.dob
     };
-
-    // Call the SOAP API method
-    //  const soapResponse = await soapClient.mySoapMethodAsync(soapInput);
-
-    // Process the SOAP response and extract the data needed for your CardHolder
-    const extractedData = {
-      // Extract the data from the SOAP response
-    };
-
-    const cardHolder = this.cardHolderRepository.create({
-      ...cardHolderData,
-      ...extractedData
-    });
-    return this.cardHolderRepository.save(cardHolder);
+    if (res[0].return.responseMessage === 'Success') {
+      await this.cardHolderRepository.save(cardHolder);
+      return res.status(200).json(res.responseMessage);
+    } else {
+      throw new BadRequestException(res[0].return.responseMessage);
+    }
   }
 
   async getCardHolder(id: number): Promise<CardHolder> {
